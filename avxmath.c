71ac3120b47f046f05b1fb94f32882d8613aa0bf
Nikolay <nmn@nmn-laptop.(none)>
2013-10-23 13:34:13 +0400

    Use SIMD functions for all elements, do not return NaN when step is not equal to element size.

--- avxmath.c
+++ avxmath.c
@@ -1,5 +1,4 @@
 #include "Python.h"
-#include "math.h"
 #include "numpy/ndarraytypes.h"
 #include "numpy/ufuncobject.h"
 #include "numpy/npy_3kcompat.h"
@@ -7,11 +6,14 @@
 /*
  * avxmath.c
  * This is the C code for avx-accelerated
- * Numpy ufunc for mathematical functions.
+ * Numpy ufuncs for mathematical functions.
  *
- * In this code we only define the ufunc for
+ * In this code we only define the ufuncs for
  * a single dtype (float64). 
  * Copyright 2013 Nikolay Nagorskiy
+ *
+ * For SIMD-accelerated mathematical functions this module uses SLEEF 
+ * library (http://shibatch.sourceforge.net/)
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -34,18 +36,17 @@ static PyMethodDef AvxmathMethods[] = {
 
 /* The loop definition must precede the PyMODINIT_FUNC. */
 typedef struct {
-    double (*mathfunc)(double);
-    vdouble (*avxfunc)(vdouble);
+    vdouble (*f)(vdouble);
     char name[10];
     char docstring[256];
-} tandem;
+} avx_func;
 
-tandem sin_tandem = {&sin, &xsin, "sin", "AVX-accelerated float64 sinus calculation"};
-tandem cos_tandem = {&cos, &xcos, "cos", "AVX-accelerated float64 cosinus calculation"};
-tandem exp_tandem = {&exp, &xexp, "exp", "AVX-accelerated float64 exponent calculation"};
+avx_func avx_sin = {&xsin, "sin", "AVX-accelerated float64 sinus calculation"};
+avx_func avx_cos = {&xcos, "cos", "AVX-accelerated float64 cosinus calculation"};
+avx_func avx_exp = {&xexp, "exp", "AVX-accelerated float64 exponent calculation"};
 
-static tandem *tandems[] = {
-    &sin_tandem, &cos_tandem, &exp_tandem,
+static avx_func *functions[] = {
+    &avx_sin, &avx_cos, &avx_exp,
 };
 
 static void double_xloop(char **args, npy_intp *dimensions,
@@ -55,39 +56,41 @@ static void double_xloop(char **args, npy_intp *dimensions,
     npy_intp n = dimensions[0];
     char *in = args[0], *out = args[1];
     npy_intp in_step = steps[0], out_step = steps[1];
-    tandem *f = (tandem *)data;
-    double (*mathfunc)(double) = f->mathfunc;
-    vdouble (*avxfunc)(vdouble) = f->avxfunc;
-    for(i = 0; i < n % VECTLENDP; i++)
-    {
-        *((double *)out) = (*mathfunc)(*(double *)in);
-        in += in_step;
-        out += out_step;
-    }
-    if(i == n) return;
+    avx_func *func = (avx_func *)data;
+    vdouble (*f)(vdouble) = func->f;
+    double tmp[VECTLENDP];
+    vdouble a;
+    int slow_n = n % VECTLENDP;
     if(in_step != sizeof(double) || out_step != sizeof(double))
+        slow_n = n;
+    for(i = 0; i < slow_n; i += VECTLENDP)
     {
-        int i;
-        out = args[1];
-        for(i = 0; i < n; i++)
+        int j;
+        for(j = 0; j < VECTLENDP && i + j < slow_n; j++)
         {
-            *((double *)out) = NAN;
-            out += out_step;
+            tmp[j] = *(double *)in;
+            in += in_step;            
         }
-        return;
-    } 
-    else 
+        a = vloadu(tmp);
+        a = (*f)(a);
+        vstoreu(tmp, a);
+        for(j = 0; j < VECTLENDP && i + j < slow_n; j++)
+        {
+            *(double *)out = tmp[j];
+            out += out_step;
+        }        
+    }
+    if(n > slow_n)
     {
-    	vdouble a;
         double *in_array = (double *)in;
         double *out_array = (double *)out;
-        for(i = 0; i < n; i += VECTLENDP)
+        for(i = 0; i < n - slow_n; i += VECTLENDP)
         {
             a = vloadu(in_array + i);
-    	    a = (*avxfunc)(a);
-	    	vstoreu(out_array + i, a);    
+        	a = (*f)(a);
+	        vstoreu(out_array + i, a);    
         }
-   }
+    }
 }
 
 static void double_sincos(char **args, npy_intp *dimensions,
@@ -97,44 +100,50 @@ static void double_sincos(char **args, npy_intp *dimensions,
     npy_intp n = dimensions[0];
     char *in = args[0], *out1 = args[1], *out2 = args[2];
     npy_intp in_step = steps[0], out1_step = steps[1], out2_step = steps[2];
-    for(i = 0; i < n % VECTLENDP; i++)
+    double tmp1[VECTLENDP], tmp2[VECTLENDP];
+    vdouble a;
+    vdouble2 b;
+    int slow_n = n % VECTLENDP;
+    if(in_step != sizeof(double) || out1_step != sizeof(double) || 
+       out2_step != sizeof(double))
     {
-        sincos(*(double *)in, (double *)out1, (double *)out2);
-        in += in_step;
-        out1 += out1_step;
-        out2 += out2_step;
-    }    
-    if(i == n) return;
-    if(in_step != sizeof(double) || out1_step != sizeof(double) || out2_step != sizeof(double))
+        slow_n = n;
+    }
+    for(i = 0; i < slow_n; i += VECTLENDP)
     {
-        int i;
-        out1 = args[1];
-        out2 = args[2];
-        for(i = 0; i < n; i++)
+        int j;
+        for(j = 0; j < VECTLENDP && i + j < slow_n; j++)
         {
-            *((double *)out1) = NAN;
-            *((double *)out2) = NAN;
-            out2 += out1_step;
-            out2 += out1_step;
+            tmp1[j] = *(double *)in;
+            in += in_step;            
         }
-        return;
-    } 
-    else 
+        a = vloadu(tmp1);
+        b = xsincos(a);
+        vstoreu(tmp1, b.x);    
+        vstoreu(tmp2, b.y);    
+        for(j = 0; j < VECTLENDP && i + j < slow_n; j++)
+        {
+            *(double *)out1 = tmp1[j];
+            *(double *)out2 = tmp2[j];
+            out1 += out1_step;
+            out2 += out2_step;
+        }        
+    }
+    if(n > slow_n)
     {
-    	vdouble a;
-        vdouble2 b;
         double *in_array = (double *)in;
         double *out_array1 = (double *)out1;
         double *out_array2 = (double *)out2;
-        for(i = 0; i < n; i += VECTLENDP)
-        {
+        for(i = 0; i < n - slow_n; i += VECTLENDP)
+        {  
             a = vloadu(in_array + i);
     	    b = xsincos(a);
 	    	vstoreu(out_array1 + i, b.x);    
-	    	vstoreu(out_array2 + i, b.y);    
+	    	vstoreu(out_array2 + i, b.y); 
         }
-   }
+    }
 }
+
 static PyUFuncGenericFunction funcs1[] = {&double_xloop};
 static PyUFuncGenericFunction funcs2[] = {&double_sincos};
 
@@ -143,18 +152,18 @@ static char types1[] = {NPY_DOUBLE, NPY_DOUBLE};
 static char types2[] = {NPY_DOUBLE, NPY_DOUBLE, NPY_DOUBLE,};
 static void *data2[] = {NULL};
 
-static void register_avx_tandems(PyObject *m)
+static void register_avx_functions(PyObject *m)
 {
     int i;
     PyObject *f, *d;
     d = PyModule_GetDict(m);
-    for(i = 0; i < sizeof(tandems)/sizeof(tandems[0]); i++)
+    for(i = 0; i < sizeof(functions)/sizeof(functions[0]); i++)
     {
         
-        f = PyUFunc_FromFuncAndData(funcs1, (void *)(tandems + i), types1, 1, 1, 1,
-                                    PyUFunc_None, tandems[i]->name,
-                                    tandems[i]->docstring, 0);
-        PyDict_SetItemString(d, tandems[i]->name, f);
+        f = PyUFunc_FromFuncAndData(funcs1, (void *)(functions + i), types1, 1, 1, 1,
+                                    PyUFunc_None, functions[i]->name,
+                                    functions[i]->docstring, 0);
+        PyDict_SetItemString(d, functions[i]->name, f);
         Py_DECREF(f);
     }
     f = PyUFunc_FromFuncAndData(funcs2, data2, types2, 1, 1, 2,
@@ -186,7 +195,7 @@ PyMODINIT_FUNC PyInit_avxmath(void)
     }
     import_array();
     import_umath();
-    register_avx_tandems(m);
+    register_avx_functions(m);
     return m;
 }
 #else
@@ -199,6 +208,6 @@ PyMODINIT_FUNC initavxmath(void)
     }
     import_array();
     import_umath();
-    register_avx_tandems(m);
+    register_avx_functions(m);
 }
 #endif
