aa382edb45116f08b9fd76536df6f1c2b042c9d1
Nikolay <nmn@nmn-laptop.(none)>
2013-10-23 13:37:51 +0400

    Improved benchmark test.
    Benchmark now uses clock function to measure CPU clock and not wall clock.
    Added relative difference output. Added support of different test range
    for different functions.

--- benchmark.py
+++ benchmark.py
@@ -5,41 +5,49 @@ import avxmath
 import time
 from numpy import random, pi
 
-COUNT=1
+COUNT=10
 
 functions = [
-    ("sin", numpy.sin, avxmath.sin),
-    ("cos", numpy.cos, avxmath.cos),
-    ("exp", numpy.exp, avxmath.exp),
+    ("sin", numpy.sin, avxmath.sin, (-1e4, 1e4)),
+    ("cos", numpy.cos, avxmath.cos, (-1e4, 1e4)),
+    ("exp", numpy.exp, avxmath.exp, (-7e2, 7e2)),
 ]
 
-def test_func(name, npfunc, avxfunc):
-    x = random.random(10000001)
-    #x = x.reshape(100,100000)
-    t = time.time()
-    for i in range(COUNT):
+def test_func(arg, name, npfunc, avxfunc):
+    t = time.clock()
+    for i in xrange(COUNT):
         y1 = npfunc(x)
-    duration1 = time.time() - t
+    duration1 = time.clock() - t
     print "numpy.%s %f sec" % (name, duration1)
 
-    t = time.time()
-    for i in range(COUNT):
+    t = time.clock()
+    for i in xrange(COUNT):
         y2 = avxfunc(x)
-    duration2 = time.time() - t
+    duration2 = time.clock() - t
     print "avxmath.%s %f sec" % (name, duration2)
 
-    print "max difference is %lg" % (y2 - y1).max()
-    print "speedup is %lg" % (duration1/duration2)
+    delta = y2 - y1;
+    rdelta = delta/y1;
+    print "Max absolute difference is %lg, relative %lg" % (
+            delta[abs(delta).argmax()], rdelta[abs(rdelta).argmax()])
+    print "Speedup is %lg" % (duration1/duration2)
 
-for name, npfunc, avxfunc in functions:
-    test_func(name, npfunc, avxfunc)
-    print 
+r = random.random(40000000);
+for name, npfunc, avxfunc, trange in functions:
+    x1, x2 = trange
+    x = x1 + r*(x2 - x1)
+    test_func(x, name, npfunc, avxfunc)
+    print
 
-x = random.random(10000001)
+x = -1e4 + r*2e4;
 ys1, yc1 = numpy.sin(x), numpy.cos(x)
 ys2, yc2 = avxmath.sincos(x)
-
-print "sincos: max difference sin is %lg" % abs(ys2 - ys1).max()
-print "sincos: max difference cos is %lg" % abs(yc2 - yc1).max()
-
+delta = ys2 - ys1
+rdelta = delta/ys1
+print "sincos sin: max absolute difference is %lg, relative %lg" % (
+            delta[abs(delta).argmax()], rdelta[abs(rdelta).argmax()])
+delta = yc2 - yc1
+rdelta = delta/yc1
+print "sincos cos: max absolute difference is %lg, relative %lg" % (
+            delta[abs(delta).argmax()], rdelta[abs(rdelta).argmax()])
 
